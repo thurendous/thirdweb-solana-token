8770c5a58b6c0c019e5a852189693e64b23de841
thurendous <thurendous2@gmail.com>
2022-10-24 01:56:17 +0900

    first commit

---
+++ scripts/deploy-token.js
@@ -0,0 +1,28 @@
+import { ThirdwebSDK } from '@thirdweb-dev/sdk/solana'
+import { config } from 'dotenv'
+config()
+
+const { PRIVATE_KEE } = process.env
+
+// initiate the SDK and pass in a signer
+const sdk = ThirdwebSDK.fromPrivateKey('devnet', PRIVATE_KEE)
+
+// Define the metadata for your program
+const metadata = {
+    symbol: 'BBT ',
+    descripton: 'best sonana token as a test token',
+    name: 'best babe token',
+    initialSupply: 100,
+}
+
+// deploy a new program from the connected wallet
+const address = await sdk.deployer.createToken(metadata)
+
+console.log(`address -> ${address}`)
+console.log(`well done! Contract deployed successfully!`)
+
+// get the total supply of the token and show it
+const token = await sdk.getToken(address)
+const supply = await token.totalSupply()
+
+console.log(`total supply -> ${supply}`)
